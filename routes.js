45485261c2e04f3919fa52e338a9c8b77ed2721a
CACUCRC <carloscu@hotmail.es>
2022-04-07 16:02:20 -0600

    basic frontend intergration

--- routes.js
+++ routes.js
@@ -7,30 +7,37 @@ const dbo = require('./connect')
 router.route("/login").post(async function (req, res) {
     const dbConnect = dbo.getDb();
 
-    dbConnect
-        .collection("userData")
-        .findOne({ userName: req.body.userName, password: req.body.password }, function (err, result) {
-            if (err) {
-                res.status(400).send("Log in failed");
-            } else {
-                res.status(200).send("Log in successful")
-            }
-        });
+    let user = await dbConnect.collection("userData").findOne({ userName: req.body.userName, password: req.body.password })
+    console.log(user);
+    if (user) {
+        res.status(200).send("Log in successful")
+    } else {
+        res.status(400).send("Log in failed")
+    }
 });
 
-router.route("/login").get(async function (req, res) {
+router.route("/generate").post(async function (req, res) {
+
+    const urlS = req.body.urlSent
+    console.log(urlS)
+
     const dbConnect = dbo.getDb();
 
-    dbConnect
-        .collection("userData")
-        .find({})
-        .toArray(function (err, result) {
-            if (err) {
-                res.status(400).send("Error fetching listings!");
-            } else {
-                res.status(200).json(result);
-            }
-        });
+    let check = await dbConnect.collection("urlData").findOne({ url: urlS })
+    if (check) {
+        res.status(200).json(check)
+    } else {
+        let urlBin = ""
+        for (var i = 0; i < urlS.length; i++) {
+            urlBin += urlS[i].charCodeAt(0).toString(2)
+        }
+        console.log(urlBin)
+        const code = parseInt(urlBin,2)
+        const codeHex = code.toString(16)
+        console.log(code)
+        console.log(codeHex.slice(6))
+        res.status(200).send(urlBin);
+    }
 });
 
 module.exports = router;
